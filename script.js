0c62ad8466b844fa0fbfe19e71610aa11054082a
Samarsalem203 <122756779+Samarsalem203@users.noreply.github.com>
2023-04-04 01:27:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // GLOBAL VARIABLES
 let tasksContainer = [];
-let box = ``;
 let mainIndex = 0;
 let drag = null;
 let clicked;
@@ -11,14 +10,11 @@ let updateBtn;
 let deleteBtn;
 let updatedTaskInput;
 let myTarget;
-let currentStatus;
-let updateInput = "";
-// let taskDiv = "";
 let taskBox;
-let addBtn = document.querySelectorAll(".addBtn");
+let addBtns = document.querySelectorAll(".addBtn");
 
-// Check local storage before add any task to show old tasks for user
-(function checkLocalStorage() {
+// Check the local storage before add any task to show the old tasks for the user
+function checkLocalStorage() {
   if (JSON.parse(localStorage.getItem("AllTasks")) == null) {
     tasksContainer = [];
   } else {
@@ -26,141 +22,127 @@ let addBtn = document.querySelectorAll(".addBtn");
     tasksContainer = JSON.parse(localStorage.getItem("AllTasks"));
     displayMyTasks();
   }
-})();
+}
+
+checkLocalStorage();
+
+function createEnterInput() {
+  // Create div to add new task
+  taskBox = document.createElement("div");
+  taskBox.className = "taskBox ";
+  mainBox.appendChild(taskBox);
+
+  //  Create input to add new task
+  taskInput = document.createElement("input");
+  taskInput.className = "taskInput";
+  taskInput.setAttribute("type", "text");
+  taskInput.value = ""; // Let input element is empty
+  taskInput.placeholder = "write your task here";
+  taskBox.appendChild(taskInput);
+
+  // Create delete btn
+  deleteBtn = document.createElement("button");
+  deleteBtn.className = "btn  del-btn"; // add class
+  deleteBtn.innerHTML = `<ion-icon name="trash" ></ion-icon>`; // put delete icon in delete btn
+  taskBox.append(deleteBtn);
+
+  deleteBtn.addEventListener("click", function () {
+    console.log("delete");
+
+    let currentEnterBtn = this.closest(".taskBox"); // taskBox DIV
+
+    currentEnterBtn.remove();
+
+    console.log(taskInput == false);
+  });
+  clicked = true;
+}
 
 // When press on add button
-addBtn.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    let box = e.target.parentElement; // Box DIV
+addBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    // console.log(ele);
+    let box = this.parentElement; // Box DIV
     console.log(box);
     mainBox = box.querySelector(".mainBox");
     taskBox = box.querySelector(".taskBox"); // Parent of task DIV
-
-    function createEnterInput() {
-      // Create div to add new task
-      taskBox = document.createElement("div");
-      taskBox.className = "taskBox ";
-      mainBox.appendChild(taskBox);
-
-      //  Create input to add new task
-      taskInput = document.createElement("input");
-      taskInput.className = "taskInput";
-      taskInput.setAttribute("type", "text");
-      taskInput.value = ""; // Let input element is empty
-      taskInput.placeholder = "write your task here";
-      taskBox.appendChild(taskInput);
-
-      // Create delete btn
-      deleteBtn = document.createElement("button");
-      deleteBtn.className = "btn  del-btn"; // add class
-      deleteBtn.innerHTML = `<ion-icon name="trash" ></ion-icon>`; // put delete icon in delete btn
-      taskBox.append(deleteBtn);
-
-      deleteBtn.addEventListener("click", (e) => {
-        console.log("delete");
-
-        let currentEnterBtn = e.target.closest(".taskBox"); // taskBox DIV
-
-        // currentEnterBtn.innerHTML = "";
-        currentEnterBtn.remove();
-
-        console.log(taskInput == false);
-      });
-      clicked = true;
+    taskInput = box.querySelector(".taskInput");
+    // *************** Add Button ****************
+    if (btn.innerHTML == "+ Add") {
+      addNewTask(btn);
     }
-    // ********************************** Add Button *******************************************************
-    // Check on input element if showed or no
-    if ((mainBox.innerHTML === "" && btn.innerHTML == "+ Add") || !taskInput) {
-      console.log("not find ");
-      createEnterInput();
-    }
-
-    // else if (taskBox.classList.contains("task") == false) {
-    //   console.log("contains");
-    //   console.log(taskBox.classList.contains("task"));
-    //   createEnterInput();
-    // if (taskInput.value == "") {
-    //   console.log("4ayfCCCCCCC");
-    //   taskInput.classList.add("borderDeleteinput");
-    // }
-    // }
-    // else if (taskInput && taskBox.classList.contains("reStyleTaskBox")) {
-    //   console.log("restyle");
-    //   // if (taskInput) {
-    //   if (taskInput.value == "") {
-    //     console.log("4ayf");
-    //     taskInput.classList.add("borderDeleteinput");
-    //   }
-
-    //   // console.log(taskInput.value);
-    //   else {
-    //     createEnterInput();
-    //     console.log("else");
-    //   }
-    // }
-    else if (
-      taskInput.value === "" &&
-      btn.innerHTML === "+ Add" &&
-      taskInput.readOnly === false
-    ) {
-      console.log("empty");
-      taskInput.classList.add("borderDeleteinput");
-      taskInput.readOnly = false;
-      clicked = true;
-    } else if (
-      taskInput.value &&
-      taskInput.readOnly === false &&
-      btn.innerHTML === "+ Add"
-    ) {
-      taskInput.classList.remove("borderDeleteinput");
-      taskInput.classList.remove("borderUpdateinput");
-      console.log(taskInput.value);
-
-      oneTask = {
-        taskStatus: taskInput.parentElement.parentElement.id,
-        taskValue: taskInput.value,
-      };
-
-      mainIndex++;
-      tasksContainer.push(oneTask); // push obj in array
-      localStorage.setItem("AllTasks", JSON.stringify(tasksContainer));
-      taskInput.readOnly = true;
-      clearTaskBox();
-      displayMyTasks();
-      console.log(tasksContainer);
-    } else if (
-      taskInput.value &&
-      taskInput.readOnly === true &&
-      btn.innerHTML === "+ Add"
-    ) {
-      createEnterInput();
-      taskInput.readOnly = false;
-    }
-
-    // ********************************** Update Button *******************************************************
+    // ************** Update Button ***************
     if (updatedTaskInput && btn.innerHTML === "Update") {
-      if (updatedTaskInput.value == "") {
-        console.log("updatedTaskInput");
-        updatedTaskInput.classList.add("borderUpdateInput");
-        clicked = false;
-      } else {
-        oneTask = {
-          taskStatus: updatedTaskInput.parentElement.parentElement.id,
-          taskValue: updatedTaskInput.value,
-        };
-        tasksContainer.splice(mainIndex, 1, oneTask); // replace the old task by the new task
-        localStorage.setItem("AllTasks", JSON.stringify(tasksContainer));
-        updatedTaskInput.readOnly = true;
-        btn.innerHTML = "+ Add";
-        btn.className = "btn addBtn ";
-        updatedTaskInput.remove();
-        clearTaskBox();
-        displayMyTasks();
-        console.log("updated", oneTask);
-      }
+      updateCurrentTask(btn);
     }
   });
 });
+
+function addNewTask(btn) {
+  if ((mainBox.innerHTML === "" && btn.innerHTML == "+ Add") || !taskInput) {
+    console.log("not find ");
+    createEnterInput();
+  } else if (
+    taskInput.value === "" &&
+    btn.innerHTML === "+ Add" &&
+    taskInput.readOnly === false
+  ) {
+    console.log("empty");
+    taskInput.classList.add("borderDeleteinput");
+    taskInput.readOnly = false;
+    clicked = true;
+  } else if (
+    taskInput.value &&
+    taskInput.readOnly === false &&
+    btn.innerHTML === "+ Add"
+  ) {
+    taskInput.classList.remove("borderDeleteinput");
+    taskInput.classList.remove("borderUpdateinput");
+    console.log(taskInput.value);
+
+    oneTask = {
+      taskStatus: taskInput.parentElement.parentElement.id,
+      taskValue: taskInput.value,
+    };
+
+    mainIndex++;
+    tasksContainer.push(oneTask); // push obj in array
+    localStorage.setItem("AllTasks", JSON.stringify(tasksContainer));
+    taskInput.readOnly = true;
+    clearTaskBox();
+    displayMyTasks();
+    console.log(tasksContainer);
+  } else if (
+    taskInput.value &&
+    taskInput.readOnly === true &&
+    btn.innerHTML === "+ Add"
+  ) {
+    createEnterInput();
+    taskInput.readOnly = false;
+  }
+}
+
+function updateCurrentTask(btn) {
+  if (updatedTaskInput.value == "") {
+    console.log("updatedTaskInput");
+    updatedTaskInput.classList.add("borderUpdateInput");
+    clicked = false;
+  } else {
+    oneTask = {
+      taskStatus: updatedTaskInput.parentElement.parentElement.id,
+      taskValue: updatedTaskInput.value,
+    };
+    tasksContainer.splice(mainIndex, 1, oneTask); // replace the old task by the new task
+    localStorage.setItem("AllTasks", JSON.stringify(tasksContainer));
+    updatedTaskInput.readOnly = true;
+    btn.innerHTML = "+ Add";
+    btn.className = "btn addBtn ";
+    updatedTaskInput.remove();
+    clearTaskBox();
+    displayMyTasks();
+    console.log("updated", oneTask);
+  }
+}
 
 function clearTaskBox() {
   mainBox = document.querySelectorAll(".mainBox");
@@ -175,45 +157,25 @@ function displayMyTasks() {
   let taskNotStarted = document.querySelector("#notStarted");
   let taskInProgress = document.querySelector("#inProgress");
   let taskCompleted = document.querySelector("#completed");
+  let box = ``;
 
   for (let i = 0; i < tasksContainer.length; i++) {
-    if (tasksContainer[i].taskStatus === "notStarted") {
-      box = `
-      <div class="taskBox reStyleTaskBox"  draggable="true" >${tasksContainer[i].taskValue}
-      
-    <button class="btn updateBtn">
-      <ion-icon name="create" id="upIcon" onClick="updateMyTask(this , ${i})"></ion-icon>
-      </button>
-      <button class="btn del-btn">
-      <ion-icon name="trash" id="delIcon" onClick="deleteMyTask(this ,${i})"></ion-icon>
-      </button>
-      </div>  `;
-      taskNotStarted.innerHTML += box;
-    } else if (tasksContainer[i].taskStatus === "inProgress") {
-      box = `
-      <div class="taskBox reStyleTaskBox"  draggable="true" >${tasksContainer[i].taskValue}
+    box = `
+    <div class="taskBox reStyleTaskBox"  draggable="true" >${tasksContainer[i].taskValue}
     
-        <button class="btn  updateBtn">
-          <ion-icon name="create"  onClick="updateMyTask(this , ${i} )"></ion-icon>
-          </button>
-          <button class="btn  del-btn">
-          <ion-icon name="trash"   onClick="deleteMyTask(this ,${i})"></ion-icon>
-        </button>
-     
-      </div>  `;
-      taskInProgress.innerHTML += box;
-    } else if (tasksContainer[i].taskStatus === "completed") {
-      box = `
-      <div class="taskBox reStyleTaskBox"  draggable="true" >${tasksContainer[i].taskValue}
-         <button class="btn updateBtn">
-          <ion-icon name="create" id="upIcon" onClick="updateMyTask(this , ${i})"></ion-icon>
-          </button>
-          <button class="btn del-btn">
-          <ion-icon name="trash" id="delIcon"  onClick="deleteMyTask(this ,${i})"></ion-icon>
-        </button>
-    
+  <button class="btn updateBtn">
+    <ion-icon name="create" id="upIcon" onClick="updateMyTask(this , ${i})"></ion-icon>
+    </button>
+    <button class="btn del-btn">
+    <ion-icon name="trash" id="delIcon" onClick="deleteMyTask(this ,${i})"></ion-icon>
+    </button>
     </div>  `;
 
+    if (tasksContainer[i].taskStatus === "notStarted") {
+      taskNotStarted.innerHTML += box;
+    } else if (tasksContainer[i].taskStatus === "inProgress") {
+      taskInProgress.innerHTML += box;
+    } else if (tasksContainer[i].taskStatus === "completed") {
       taskCompleted.innerHTML += box;
     }
     mainIndex = i + 1;
@@ -225,15 +187,19 @@ function displayMyTasks() {
 //******************************DRAG & DROP ITEMS ***********************/
 
 // ************* TASK *************************
-function dragItem() {
+function dragTask() {
   let tasks = document.querySelectorAll(".taskBox");
-  let mainBox = document.querySelectorAll(".mainBox"); // parent of task box
-
-  //********* DRAG START ************
-
   tasks.forEach((task) => {
-    task.addEventListener("dragstart", (e) => {
+    task.addEventListener("dragstart", () => {
       myTarget = task;
+
+      // get task index
+      findIndex = tasksContainer.findIndex((ele) => {
+        if (ele.taskValue === task.innerText) {
+          return ele;
+        }
+      });
+
       drag = myTarget;
       task.classList.add("dragging");
     });
@@ -293,8 +259,10 @@ function dragItem() {
       });
     });
   });
+}
 
-  //****************** BOX OF TASKS ************************* */
+function dragOver_Leave_drop() {
+  let mainBox = document.querySelectorAll(".mainBox"); // parent of task box
   mainBox.forEach((box) => {
     //****** DRAG OVER ***************
     box.addEventListener("dragover", (e) => {
@@ -311,33 +279,41 @@ function dragItem() {
     });
 
     //****** DROP **************
-    box.addEventListener("drop", () => {
-      box.style.background = "rgb(19, 155, 121)";
-      box.appendChild(myTarget);
+    box.addEventListener("drop", function () {
+      let boxStatus = this.id;
+      this.style.background = "rgb(19, 155, 121)";
 
-      let currentValue = currentBox.innerText;
-      myTarget.disabled = true;
+      this.appendChild(myTarget);
+
+      tasksContainer[findIndex].taskStatus = boxStatus; // update task status
+      localStorage.setItem("AllTasks", JSON.stringify(tasksContainer)); // push update to local sorage
+
+      // let currentValue = currentBox.innerText;
+      // myTarget.disabled = true;
     });
   });
 }
 
-//*********************** UPDATE ***********************************/
-// To update my task
+function dragItem() {
+  dragTask();
+  dragOver_Leave_drop();
+}
 
+//************* To update my task ************
 function updateMyTask(element, index) {
   taskBox = element.parentElement.parentElement;
   let box = taskBox.closest(".box");
   let taskValue = taskBox.innerText; // store the task and display it in the input element as below
-  // let mainBox = document.querySelectorAll(".mainBox");
-  let test = taskBox.parentElement.contains(updatedTaskInput);
+  let updatedEle = taskBox.parentElement.contains(updatedTaskInput);
 
-  if (!test) {
+  if (!updatedEle) {
     console.log("not");
     clicked = true;
     taskBox.removeAttribute("draggable");
     taskBox.innerHTML = `<input type="text"/>`; // put input element only in the div task to the user can update his task
     taskBox.firstElementChild.classList.add("taskInput");
     taskBox.firstElementChild.value = taskValue; // add current task to the new input element to update it as above
+    taskBox.firstElementChild.select();
     taskBox.classList.remove("reStyleTaskBox");
     updatedTaskInput = taskBox.firstElementChild; // to select the task  what i want to update it
     let addBtnStyle = box.querySelector(".addBtn");
@@ -351,14 +327,12 @@ function updateMyTask(element, index) {
   } else {
     clicked = false;
   }
-  console.log(updatedTaskInput);
+
   return updatedTaskInput;
 }
 
-// ********************DELETE***************************************************
-// Delete my task
-function deleteMyTask(element, index) {
-  console.log(element);
+// ********* Delete my task *******************
+function deleteMyTask(index) {
   tasksContainer.splice(index, 1);
   localStorage.setItem("AllTasks", JSON.stringify(tasksContainer));
   clearTaskBox();
